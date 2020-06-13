0a6e15a69b727b856fecf1b7c847e70efa097102
guidoasbun <gdrodrigo8@gmail.com>
2020-06-13 16:20:25 -0700

    converted to https

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ let city = ''
 
 let displayWeather = function(x) {
 
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${x}&units=imperial&APPID=bad2b3e36cb846608fb20772133c3d58`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${x}&units=imperial&APPID=bad2b3e36cb846608fb20772133c3d58`)
   .then(r => r.json())
   .then(({ main, wind, name, weather, coord}) => {
 
@@ -19,7 +19,7 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=${x}&units=imperial&APPI
     document.getElementById('humidity').textContent = ('Humidity:  ' + humidity +'%')
     document.getElementById('windSpeed').textContent = ('Wind Speed:  ' + windSpeed +'mph')
 
-    fetch(`http://api.openweathermap.org/data/2.5/uvi?appid=bad2b3e36cb846608fb20772133c3d58&lat=${lat}&lon=${lon}`)
+    fetch(`https://api.openweathermap.org/data/2.5/uvi?appid=bad2b3e36cb846608fb20772133c3d58&lat=${lat}&lon=${lon}`)
       .then(r => r.json())
       .then(({value}) => {
         let condition
@@ -33,7 +33,7 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=${x}&units=imperial&APPI
         document.getElementById('uvIndex').innerHTML = (`<p>UV Index: <span class="badge ${condition}">${value}</span></p>`)
       })
   })
-  fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${x}&units=imperial&APPID=bad2b3e36cb846608fb20772133c3d58`)
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${x}&units=imperial&APPID=bad2b3e36cb846608fb20772133c3d58`)
       .then(r => r.json())
       .then(({list}) =>{
 
@@ -42,7 +42,7 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=${x}&units=imperial&APPI
         let dayOneTemp = list[2].main.temp
         let dayOneHumidity = list[2].main.humidity
         document.getElementById('dayOneDate').textContent = (dayOneDate)
-        document.getElementById('dayOneIcon').innerHTML = (`<img src="http://openweathermap.org/img/wn/${dayOneIcon}.png" alt="Weather Icon">`)
+        document.getElementById('dayOneIcon').innerHTML = (`<img src="https://openweathermap.org/img/wn/${dayOneIcon}.png" alt="Weather Icon">`)
         document.getElementById('dayOneTemp').textContent = ('Temp:  ' + dayOneTemp + " F")
         document.getElementById('dayOneHumidity').textContent = ('Humidity:  ' + dayOneHumidity + ' %')
 
@@ -51,7 +51,7 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=${x}&units=imperial&APPI
         let dayTwoTemp = list[10].main.temp
         let dayTwoHumidity = list[10].main.humidity
         document.getElementById('dayTwoDate').textContent = (dayTwoDate)
-        document.getElementById('dayTwoIcon').innerHTML = (`<img src="http://openweathermap.org/img/wn/${dayTwoIcon}.png" alt="Weather Icon">`)
+        document.getElementById('dayTwoIcon').innerHTML = (`<img src="https://openweathermap.org/img/wn/${dayTwoIcon}.png" alt="Weather Icon">`)
         document.getElementById('dayTwoTemp').textContent = ('Temp:  ' + dayTwoTemp + " F")
         document.getElementById('dayTwoHumidity').textContent = ('Humidity:  ' + dayTwoHumidity + ' %')
 
@@ -60,7 +60,7 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=${x}&units=imperial&APPI
         let dayThreeTemp = list[18].main.temp
         let dayThreeHumidity = list[18].main.humidity
         document.getElementById('dayThreeDate').textContent = (dayThreeDate)
-        document.getElementById('dayThreeIcon').innerHTML = (`<img src="http://openweathermap.org/img/wn/${dayThreeIcon}.png" alt="Weather Icon">`)
+        document.getElementById('dayThreeIcon').innerHTML = (`<img src="https://openweathermap.org/img/wn/${dayThreeIcon}.png" alt="Weather Icon">`)
         document.getElementById('dayThreeTemp').textContent = ('Temp:  ' + dayThreeTemp + " F")
         document.getElementById('dayThreeHumidity').textContent = ('Humidity:  ' + dayThreeHumidity + ' %')
 
@@ -69,7 +69,7 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=${x}&units=imperial&APPI
         let dayFourTemp = list[26].main.temp
         let dayFourHumidity = list[26].main.humidity
         document.getElementById('dayFourDate').textContent = (dayFourDate)
-        document.getElementById('dayFourIcon').innerHTML = (`<img src="http://openweathermap.org/img/wn/${dayFourIcon}.png" alt="Weather Icon">`)
+        document.getElementById('dayFourIcon').innerHTML = (`<img src="https://openweathermap.org/img/wn/${dayFourIcon}.png" alt="Weather Icon">`)
         document.getElementById('dayFourTemp').textContent = ('Temp:  ' + dayFourTemp + " F")
         document.getElementById('dayFourHumidity').textContent = ('Humidity:  ' + dayFourHumidity + ' %')
 
@@ -78,7 +78,7 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=${x}&units=imperial&APPI
         let dayFiveTemp = list[34].main.temp
         let dayFiveHumidity = list[34].main.humidity
         document.getElementById('dayFiveDate').textContent = (dayFiveDate)
-        document.getElementById('dayFiveIcon').innerHTML = (`<img src="http://openweathermap.org/img/wn/${dayFiveIcon}.png" alt="Weather Icon">`)
+        document.getElementById('dayFiveIcon').innerHTML = (`<img src="https://openweathermap.org/img/wn/${dayFiveIcon}.png" alt="Weather Icon">`)
         document.getElementById('dayFiveTemp').textContent = ('Temp:  ' + dayFiveTemp + " F")
         document.getElementById('dayFiveHumidity').textContent = ('Humidity:  ' + dayFiveHumidity + ' %')
 
